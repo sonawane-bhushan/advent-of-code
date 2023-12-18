6e9814647f5369d32a57531986783df2c0b38918
Bhushan Sonawane <bhushan.sonawane@thoughtworks.com>
2023-12-18 22:54:33 +0530

    added advent of code 2023 day 16

---
+++ AOC 2023/advent-16-2.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import sys
+
+sys.setrecursionlimit(1000000)
+
+p = Path(__file__).with_name('advent-16-data.txt')
+
+with p.open("r") as f:
+    input_array = f.read().splitlines()
+
+mirrors = {
+    '.': {'R': ['R'], 'L': ['L'], 'U': ['U'], 'D': ['D']},
+    '-': {'R': ['R'], 'L': ['L'], 'U': ['R', 'L'], 'D': ['R', 'L']},
+    '|': {'R': ['U', 'D'], 'L': ['U', 'D'], 'U': ['U'], 'D': ['D']},
+    '/': {'R': ['U'], 'L': ['D'], 'U': ['R'], 'D': ['L']},
+    '\\': {'R': ['D'], 'L': ['U'], 'U': ['L'], 'D': ['R']}
+}
+
+directions = {
+    'R': [0, 1],
+    'L': [0, -1],
+    'U': [-1, 0],
+    'D': [1, 0]
+}
+
+
+def count(start):
+    visitedList = set()
+
+    def illuminate(x: int, y: int, direction: str):
+        if (x, y, direction) in visitedList:
+            return
+        visitedList.add((x, y, direction))
+
+        mirror = input_array[x][y]
+        for nextDirection in mirrors[mirror][direction]:
+            nextPoint = directions[nextDirection]
+            nextX = x + nextPoint[0]
+            nextY = y + nextPoint[1]
+            if nextX in range(len(input_array)) and nextY in range(len(input_array[0])):
+                illuminate(nextX, nextY, nextDirection)
+        
+    illuminate(start[0], start[1], start[2])
+    return len(set([(x, y) for x, y, _ in visitedList]))
+    
+startingPoints = []
+
+for x in range(len(input_array)):
+    startingPoints.append((x, 0, 'R'))
+    startingPoints.append((x, len(input_array[0]) - 1, 'L'))
+
+for y in range(len(input_array[0])):
+    startingPoints.append((0, y, 'D'))
+    startingPoints.append((len(input_array) - 1, y, 'U'))
+
+print(max(count(point) for point in startingPoints))
