e94c34fc0ec44ed9f8eb832c1e8235c5d146e363
Bhushan Sonawane <bhushansonawane@Bhushans-MacBook-Pro.local>
2024-12-01 16:07:20 +0530

    added advent of code 2024 day 1

---
+++ AOC 2024/advent-1-2.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+p = Path(__file__).with_name('advent-1-data.txt')
+
+l1, l2 = [], []
+
+with p.open("r") as f:
+        input_array = f.read().splitlines() 
+
+for i in input_array:
+    d = i.split('   ')
+    l1.append(int(d[0]))
+    l2.append(int(d[1]))
+
+sum = 0
+
+for i in range(len(l1)):
+    sum += l1[i] * l2.count(l1[i])
+
+print(sum)
